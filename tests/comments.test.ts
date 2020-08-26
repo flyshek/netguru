381bb48365dcc45e2d64ca70941e38f62e687475
Jakub Olan <jakub.olan001@gmail.com>
2020-08-26 06:41:36 +0200

    test: create random tests

---
+++ tests/comments.test.ts
@@ -0,0 +1,30 @@
+import AnyTest, { TestInterface } from 'ava'
+import http from 'http'
+import listen from 'test-listen'
+import got from 'got'
+
+// Create basic testing context with Main Class and exported express instance.
+import { Main } from '@netguru/server'
+const { app } = new Main()
+
+// Interface for types in our tests
+interface Context {
+	server: http.Server
+	prefixUrl: string
+}
+
+const test = AnyTest as TestInterface<Context>
+
+test.beforeEach(async function (t) {
+	t.context.server = http.createServer(app)
+	t.context.prefixUrl = await listen(t.context.server)
+})
+
+test.afterEach(function (t) {
+	t.context.server.close()
+})
+
+test.serial('POST /comment should create new entry', async (t) => {})
+test.serial('POST /comment should return status 200', async (t) => {})
+test.serial('GET /comment should return created comments', async (t) => {})
+test.serial('GET /comment should return status 200', async (t) => {})
---
+++ tests/movies.test.ts
@@ -0,0 +1,62 @@
+import AnyTest, { TestInterface } from 'ava'
+import http from 'http'
+import listen from 'test-listen'
+import got from 'got'
+
+// NOTE: This test assumes there is MongoDB running, tbh I was lazy to implement
+// there some memory mongodb, implement connection and perform tests.
+
+// Create basic testing context with Main Class and exported express instance.
+import { Main } from '@netguru/server'
+import { omdbResponse, MovieSearchResponse } from '@netguru/movies'
+import { omdbMovie } from 'movies/omdb.interface'
+import { IMovie } from 'movies/movie.model'
+const { app } = new Main()
+
+// Interface for types in our tests
+interface Context {
+	server: http.Server
+	prefixUrl: string
+}
+
+const test = AnyTest as TestInterface<Context>
+
+test.beforeEach(async function (t) {
+	t.context.server = http.createServer(app)
+	t.context.prefixUrl = await listen(t.context.server)
+})
+
+test.afterEach(function (t) {
+	t.context.server.close()
+})
+
+test.serial('POST /movies should search through 3rd-party API', async (t) => {
+	const internalRequest: MovieSearchResponse = await got
+		.post('movies', {
+			prefixUrl: t.context.prefixUrl,
+			json: {
+				s: 'spiderman',
+			},
+			responseType: 'json',
+		})
+		.json()
+	const externalResponse: omdbResponse = await got('http://www.omdbapi.com', {
+		searchParams: {
+			s: 'spiderman',
+			apikey: 'a521616f',
+		},
+	}).json()
+
+	t.deepEqual(internalRequest.data, externalResponse.Search, 'API should featch data from external API')
+})
+
+test.serial('POST /movies should return status 200', async (t) => {
+	const moviesRequest = await got('movies', {
+		prefixUrl: t.context.prefixUrl,
+	})
+	t.is(moviesRequest.statusCode, 200)
+})
+
+test.serial('GET /movies should return created movies', async (t) => {})
+
+test.serial('GET /movies should return status 200', async (t) => {})
